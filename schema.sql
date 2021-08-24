fc6313dc4f8b26b49ead0eb2b4bcba6b45cbcb10
Aubri Henley <aubri.henley@gmail.com>
2021-08-24 19:37:52 -0400

    completed schema

--- schema.sql
+++ schema.sql
@@ -9,3 +9,28 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    on delete set NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id) 
+    on delete set NULL,
+    manager_id int,
+    manager_id REFERENCES employee(id)
+    on delete set NULL
+);
+
+
+
