df0359eb3092d5640c07e73526ad48e480ca024f
Aubri Henley <aubri.henley@gmail.com>
2021-08-25 13:41:01 -0400

    added seeds to seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+
+INSERT INTO roles (department_id, title, salary)
+VALUES (1, "Sales Lead", 100000),
+       (1, "Sales Person", 80000),
+       (2, "Lead Engineer", 150000),
+       (2, "Software Engineer", 120000),
+       (3, "Account Manager", 160000),
+       (3, "Accountant", 125000),
+       (4, "Legal Team Lead", 250000),
+       (4, "Lawyer", 190000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ("John", "Doe", 1, null),
+       ("Mike", "Chan", 2, 1),
+       ("Ashley", "Rodriquez", 3, null),
+       ("Kevin", "Tupik", 4, 3),
+       ("Kunal", "Singh", 5, null),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 7, null),
+       ("Tom", "Allen", 8, 7);
